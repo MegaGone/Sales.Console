407681ba0cb2b4feb07174adc2e598a2274c7a4c
Javier Martínez <jmartinez@develsystems.com>
2022-07-03 18:01:31 -0600

    Product operations

--- src/app/pages/sales/new-sale/new-sale.component.ts
+++ src/app/pages/sales/new-sale/new-sale.component.ts
@@ -15,6 +15,17 @@ export class NewSaleComponent implements OnInit {
   public Clients   : IClient[]
   public PayMethods: IPayMethod[];
 
+  public existArr : any[] = [
+    {
+      id: 0,
+      value: 'Si'
+    },
+    {
+      id: 1,
+      value: 'No'
+    }
+  ]
+
   public salesForm: FormGroup;
 
   constructor(private fb: FormBuilder) { 
@@ -32,21 +43,64 @@ export class NewSaleComponent implements OnInit {
    */
   initForm() {
     this.salesForm = this.fb.group({
-      saleNo    : ['', Validators.required],
-      date      : ['', Validators.required],
-      payMethod : [1, Validators.required],
-      days      : [{ value: '', disabled: true }],
-      print     : [false],
-      clientNo  : ['', Validators.required],
-      clientName: ['', Validators.required],
-      phone     : ['', Validators.required],
-      address   : ['', Validators.required],
-      cupo      : ['', Validators.required],
-      price     : ['', Validators.required],
-      products  : this.fb.array([])
+      saleNo        : ['', Validators.required],
+      date          : ['', Validators.required],
+      payMethod     : [1, Validators.required],
+      days          : [{ value: '', disabled: true }],
+      print         : [false],
+      clientNo      : ['', Validators.required],
+      clientName    : ['', Validators.required],
+      phone         : ['', Validators.required],
+      address       : ['', Validators.required],
+      cupo          : ['', Validators.required],
+      price         : ['', Validators.required],
+      itemCode      : [''],
+      itemDesc      : [''],
+      itemquantity  : [''],
+      itemPrice     : [''],
+      totalValue    : [''],
+      discItem      : [''],
+      valueDiscItem : [''],
+      minPrice      : [''],
+      exist         : [true],
+      products      : this.fb.array([], Validators.required)
     })
 
     this.listenFormChanges();
+  }
+
+  addProduct() {
+    const product: IProduct = {
+      id      : '',
+      code    : this.Form.itemCode.value,
+      item    : this.Form.itemDesc.value,
+      quantity: this.Form.itemquantity.value,
+      price   : this.Form.itemPrice.value,
+      subTotal: this.Form.totalValue.value,
+      discount: this.Form.discItem.value
+    }
+
+    this.Products.push(product);
+
+    this.Form.itemCode.setValue('');
+    this.Form.itemDesc.setValue('');
+    this.Form.itemquantity.setValue('');
+    this.Form.itemPrice.setValue('');
+    this.Form.totalValue.setValue('');
+    this.Form.discItem.setValue('');
+  }
+
+  deleteProduct(i: number) {
+    this.Products.splice(i, 1);
+  }
+
+  editProduct(product: IProduct) {
+    this.Form.itemCode.setValue(product.code);
+    this.Form.itemDesc.setValue(product.discount);
+    this.Form.itemquantity.setValue(product.quantity);
+    this.Form.itemPrice.setValue(product.price);
+    this.Form.totalValue.setValue(product.subTotal);
+    this.Form.discItem.setValue(product.discount);
   }
 
   /**
